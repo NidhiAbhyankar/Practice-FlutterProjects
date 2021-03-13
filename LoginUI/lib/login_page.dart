8c0269ec519ce7c5f674a1fb67d57f94a5944ded
NidhiAbhyka <nidhiabhyankar@mail.com>
2021-03-13 18:26:58 +0530

    ongoing

--- LoginUI/lib/login_page.dart
+++ LoginUI/lib/login_page.dart
@@ -18,15 +18,63 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
 
-    final  email = TextFormField(
+    final email = TextFormField(
       keyboardType: TextInputType.emailAddress,
       autofocus: false,
       initialValue: 'abc@gmail.com',
       decoration: InputDecoration(
         hintText: 'Email',
-        
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
       ),
     );
-    return Container();
+
+    final password = TextFormField(
+      autofocus: false,
+      initialValue: 'some password',
+      obscureText: true,
+      decoration: InputDecoration(
+          hintText: 'Password',
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(32.0),
+          )),
+    );
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(30.0),
+        shadowColor: Colors.lightBlueAccent.shade100,
+        elevation: 5.0,
+        child: MaterialButton(
+          minWidth: 200.0,
+          height: 42.0,
+          onPressed: () {},
+          color: Colors.lightBlueAccent,
+          child: Text(
+            'Log In',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+
+    final forgotLabel = FlatButton(
+      onPressed: (){},
+      child: Text('Forgot Password?', style: TextStyle(color: Colors.black54)),
+    );
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body:Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left:24.0),
+          children: <Widget>[
+            
+          ],
+        ),)
+    );
   }
 }
